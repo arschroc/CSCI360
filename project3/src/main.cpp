4f0103dbce1ce5924135d216e46132ae28c085ab
student <arschroc@usc.edu>
2017-11-19 20:23:41 -0800

    percentages correct

--- project3/src/main.cpp
+++ project3/src/main.cpp
@@ -5,6 +5,7 @@
 #include "mnist_utils.hpp"
 #include "bitmap.hpp"
 #include <sstream>
+#include <math.h>
 #define MNIST_DATA_DIR "../mnist_data"
 int main(int argc, char* argv[]) {
     //Read in the data set from the files
@@ -83,7 +84,8 @@ int main(int argc, char* argv[]) {
         {
             if (static_cast<int>(trainImages[i][j]) == 1)
             {
-                numFeaturesGivenC[trainLabels[i]][j]++;
+                int classValue = trainLabels[i];
+                numFeaturesGivenC[classValue][j]++;
             }
         }
     }
@@ -105,35 +107,121 @@ int main(int argc, char* argv[]) {
      */
 
     //For each test image Ti compute the probability that it belongs to each class
-    double probabilityOfClass[numLabels] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    
+    
     double denominatorForC = 1.0;
+    int totalCorrect = 0;
 
     for (int i = 0; i < numFeatures; ++i)
     {
         denominatorForC *= 0.5;
     }
 
-    for (int i = 0; i < numLabels; ++i)
+    for (int i = 0; i < testImages.size(); ++i)
     {
-        double numeratorForC = priorProbabilities[i];
-        for (int j = 0; j < numFeatures; ++j)
+        double probabilityOfClass[numLabels] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        for (int c = 0; c < numLabels; ++c)
         {
-            double tempProb = 1.0;
-            if (static_cast<int>(testImages[i][j]) == 1)
+            double numeratorForC = priorProbabilities[c];
+            for (int j = 0; j < numFeatures; ++j)
             {
-                tempProb = percentFeatureGivenC[i][j];
+                double tempProb = 1.0;
+                if (static_cast<int>(testImages[i][j]) == 1)
+                {
+                    tempProb = percentFeatureGivenC[c][j];
+                }
+                else {
+                    tempProb = 1.0 - percentFeatureGivenC[c][j];
+                }
+                numeratorForC *= tempProb;
             }
-            else {
-                tempProb = 1.0 - percentFeatureGivenC[i][j];
-            }
-            numeratorForC *= tempProb;
+            probabilityOfClass[c] = numeratorForC / denominatorForC;
         }
-        probabilityOfClass[i] = numeratorForC / denominatorForC;
+
+
+        double max = probabilityOfClass[0];
+        int index = 0;
+        for (int c = 1; c < numLabels; ++c)
+        {
+            if(probabilityOfClass[c] > max) {
+                max = probabilityOfClass[c];
+                index = c;
+            }
+        }
+
+        //std::cout << "Predicted: " << index << std::endl;
+        //std::cout << "Actual: " << static_cast<int>(testLabels[i]) << std::endl;
+
+        //check correct
+        if (index == static_cast<int>(testLabels[i])) 
+        {
+            totalCorrect++;
+        } 
     }
+
+    double percentage = (double)totalCorrect / testImages.size();
+    percentage *= 100;
+    std::cout << "Percent correct: " << percentage << std::endl;
+    
+    
 
 
     //Classify Ti as belonging to class ci that maximizes the following expression:
     //Maxc (Sum (logPl(Fj= tij | C = c) + logP(C = c) ) )
+    
+    totalCorrect = 0;
+
+    std:: cout << "\n";
+    std:: cout << "Maxc (Sum (logPl(Fj= tij | C = c) + logP(C = c) ) )" << std::endl;
+    for (int i = 0; i < testImages.size(); ++i)
+    {
+        double probabilityOfClassPart2[numLabels] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        for (int c = 0; c < numLabels; ++c)
+        {
+            for (int j = 0; j < numFeatures; ++j)
+            {
+                double tempProb = 1.0;
+                if (static_cast<int>(testImages[i][j]) == 1)
+                {
+                    tempProb = percentFeatureGivenC[c][j];
+                }
+                else {
+                    tempProb = 1.0 - percentFeatureGivenC[c][j];
+                }
+
+                tempProb = log(tempProb);
+                probabilityOfClassPart2[c] += tempProb;
+            }
+            probabilityOfClassPart2[c] += log(priorProbabilities[c]);
+        } 
+
+        double max = probabilityOfClassPart2[0];
+        int index = 0;
+        for (int c = 1; c < numLabels; ++c)
+        {
+            if(probabilityOfClassPart2[c] > max) {
+                max = probabilityOfClassPart2[c];
+                index = c;
+            }
+        }
+
+        //std::cout << "Predicted: " << index << std::endl;
+        //std::cout << "Actual: " << static_cast<int>(testLabels[i]) << std::endl;
+
+        //check correct
+        if (index == static_cast<int>(testLabels[i])) 
+        {
+            totalCorrect++;
+        }    
+    }
+
+    percentage = (double)totalCorrect / testImages.size();
+    percentage *= 100;
+    std::cout << "Percent correct: " << percentage << std::endl;
+    
+
 
 
 
